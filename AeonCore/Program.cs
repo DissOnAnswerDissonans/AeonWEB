c5ced16c5f7f572e712f4cf6a68088126b4e7e80
Tupotrof <supertrof101@gmail.com>
2021-10-08 12:22:43 +0500

    First Commit

---
+++ AeonCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AeonCore
+{
+    class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Aeon!");
+        }
+    }
+}
